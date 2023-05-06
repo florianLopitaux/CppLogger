42ad5d0cba58b667b0dab4700762b4c4fbc8e8bd
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2023-05-06 15:08:08 +0200

    Created the LoggerColor enum

---
+++ src/include/LoggerColor.h
@@ -0,0 +1,26 @@
+#ifndef COLORS_LOGGER_H
+#define COLORS_LOGGER_H
+
+
+namespace nsCppLogger {
+    enum LoggerColor {
+        BLACK,
+        BLUE,
+        GREEN,
+        AQUA,
+        RED,
+        PURPLE,
+        YELLOW,
+        WHITE,
+        GRAY,
+        LIGHT_BLUE,
+        LIGHT_GREEN,
+        LIGHT_AQUA,
+        LIGHT_RED,
+        LIGHT_PURPLE,
+        LIGHT_YELLOW,
+        BRIGHT_WHITE
+    };
+}
+
+#endif
